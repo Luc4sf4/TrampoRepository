6ab0b8a8390b51806dc7724fc962bdcb91d3839f
Lucas Fernandes Dias <superlucas2005@gmail.com>
2022-01-12 10:34:51 -0300

    commitando pra salvar essas ultimas alterações.

--- springboot-rest-api-sample/src/main/resources/static/getParameter.js
+++ springboot-rest-api-sample/src/main/resources/static/getParameter.js
@@ -5,12 +5,18 @@
 				url : "http://localhost:8081/api/parametros/id",
 				data : "id=" + 3,
 				success : function(response) {
+				console.log(response);
 					$("#id").val(response.id);
-					$('#siteTitle').text(response.sitetitle);
-					$('#subTitle').text(response.subtitle1);
-					$('#Txt').text(response.txt1);
-					$('#album_title').text(response.title_album);
-					$('#video_title').text(response.title_video);
+					$('#siteTitle').text(response.sitetitle)       ;
+					$('#subTitle').text(response.subtitle1)        ;
+					$('#Txt').text(response.txt1)                  ;
+					$('#album_title').text(response.title_album)   ;
+					$('#video_title').text(response.title_video)   ;
+					$('#parametro_145').text(response.parametro145);
+					$('#parametro_152').text(response.parametro152);
+					
+					
+					
 					
 				}
 
