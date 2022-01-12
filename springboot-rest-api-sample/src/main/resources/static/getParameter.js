dc48f1d543b76e2a59998eb32900f404c48dd324
Lucas Fernandes Dias <superlucas2005@gmail.com>
2022-01-12 11:59:53 -0300

    inserir outros 8 parametros no front

--- springboot-rest-api-sample/src/main/resources/static/getParameter.js
+++ springboot-rest-api-sample/src/main/resources/static/getParameter.js
@@ -14,12 +14,16 @@
 					$('#video_title').text(response.title_video)   ;
 					$('#parametro_145').text(response.parametro145);
 					$('#parametro_152').text(response.parametro152);
-					
-					
-					
-					
+					$('#parametro_190').text(response.parametro190);
+					$('#parametro_547').text(response.parametro547);
+					$('#parametro_567').text(response.parametro567);
+					$('#parametro_765').text(response.parametro765);
+					$('#parametro_786').text(response.parametro786);
+					$('#parametro_789').text(response.parametro789);
+					$('#parametro_899').text(response.parametro899);
+					$('#parametro_988').text(response.parametro988);
 				}
-
+				
 			}).fail(function(xhr, status, errorThrown) {
 				alert("Erro ao Buscar Usuario por id: " + xhr.reponseText);
 
