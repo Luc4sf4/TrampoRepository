c04ea8bf5ad1b4f2940109b97ede2cb9c79b04db
Lucas Fernandes Dias <superlucas2005@gmail.com>
2022-01-14 11:20:36 -0300

    youtube player inseridos

--- springboot-rest-api-sample/src/main/resources/static/getParameter.js
+++ springboot-rest-api-sample/src/main/resources/static/getParameter.js
@@ -6,27 +6,51 @@
 				data : "id=" + 3,
 				success : function(response) {
 				console.log(response);
-					$("#id").val(response.id);
-					$('#siteTitle').text(response.sitetitle)       ;
-					$('#subTitle').text(response.subtitle1)        ;
-					$('#Txt').text(response.txt1)                  ;
-					$('#album_title').text(response.title_album)   ;
-					$('#video_title').text(response.title_video)   ;
-					$('#parametro_145').text(response.parametro145);
-					$('#parametro_152').text(response.parametro152);
-					$('#parametro_190').text(response.parametro190);
-					$('#parametro_547').text(response.parametro547);
-					$('#parametro_567').text(response.parametro567);
-					$('#parametro_765').text(response.parametro765);
-					$('#parametro_786').text(response.parametro786);
-					$('#parametro_789').text(response.parametro789);
-					$('#parametro_899').text(response.parametro899);
-					$('#parametro_988').text(response.parametro988);
+					$("#id").val(response.id)                             ;
+					$('#siteTitle').text(response.sitetitle)              ;
+					$('#subTitle').text(response.subtitle1)               ;
+					$('#Txt').text(response.txt1)                         ;
+					$('#album_title').text(response.title_album)          ;
+					$('#video_title').text(response.title_video)          ;
+					$('#parametro_145').text(response.parametro145)       ;
+					$('#parametro_152').text(response.parametro152)       ;
+					$('#parametro_190').text(response.parametro190)       ;
+					$('#parametro_547').text(response.parametro547)       ;
+					$('#parametro_567').text(response.parametro567)       ;
+					$('#parametro_786').text(response.parametro786)       ;
+					$('#parametro_899').text(response.parametro899)       ;
+					
+					
+				    $('#parametro_789').attr('src', response.parametro789);
+				    $('#parametro_988').attr('src', response.parametro988);
+				    $('#parametro_765').attr('src', response.parametro765);				
+			
 				}
+				
 				
 			}).fail(function(xhr, status, errorThrown) {
 				alert("Erro ao Buscar Usuario por id: " + xhr.reponseText);
 
-			})
-		});
+			});
+			
+			
+			//appends an "active" class to .popup and .popup-content when the "Open" button is clicked
+$(".open").on("click", function() {
+
+  $(".popup-overlay, .popup-content").addClass("active");
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+});
+			
+			
+			
+			
+				});	
+			
+		
+			
+
 	
